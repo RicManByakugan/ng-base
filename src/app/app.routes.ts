943568f6317bf197971f1ccaebdc7fe04df0fd05
Ratovonirina <eric.ratovonirina@gmail.com>
2024-09-30 18:15:07 +0200

    Base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      },
+      {
+        path: 'product',
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+      }
+];
